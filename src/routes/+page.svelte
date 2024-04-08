0b6b79fda501859c0bcbbc664a49d5e737544e1c
incrypted-battles <incrypted.battles@gmail.com>
2024-04-07 22:29:05 -0400

    logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,24 +7,24 @@
 </svelte:head>
 
 <div
-	class="flex flex-col min-h-screen bg-[#E5E5E5] bg-[url('/bg.webp')] bg-contain bg-no-repeat bg-[center_top_60px]"
+	class="flex min-h-screen flex-col bg-[#E5E5E5] bg-[url('/bg.webp')] bg-contain bg-[center_top_60px] bg-no-repeat"
 >
-	<main class="grow max-w-screen-xl mx-auto px-6 w-full flex flex-col items-center">
-		<h1 class="text-[60px] center font-bold font-jakarta text-black mt-16 md:mt-40 mb-8">
-			ChainPay
+	<main class="mx-auto flex w-full max-w-screen-xl grow flex-col items-center px-6">
+		<h1 class="center mb-8 mt-16 md:mt-40">
+			<img src="/logo.png" alt="ChainPay" class="w-[266px] md:w-[366px]" />
 		</h1>
-		<h3 class="text-xl center mb-6 max-w-lg text-center text-[#5A5A5A] font-roboto">
+		<h3 class="center mb-6 max-w-lg text-center font-roboto text-xl text-[#5A5A5A]">
 			Simplifying Invoice Creation and Payments in the Solana Ecosystem
 		</h3>
 		<a
 			href="#"
-			class="bg-[#17FB9B] hover:bg-[#00ee99] text-[#050505] font-roboto font-bold text-xl py-5 px-12 rounded-lg"
+			class="rounded-lg bg-[#17FB9B] px-12 py-5 font-roboto text-xl font-bold text-[#050505] hover:bg-[#00ee99]"
 			>Download Pitch Deck</a
 		>
-		<img src="/pay.webp" alt="ChainPay" class="max-w-[960px] w-full mt-16 mb-16 mx-auto block" />
+		<img src="/pay.webp" alt="ChainPay" class="mx-auto mb-16 mt-16 block w-full max-w-[960px]" />
 	</main>
 	<footer
-		class="text-center max-w-screen-xl mx-auto px-6 w-full my-4 font-roboto text-black text-base md:text-lg"
+		class="mx-auto my-4 w-full max-w-screen-xl px-6 text-center font-roboto text-xs text-black md:text-lg"
 	>
 		Copyright © 2024 ChainPay. All rights reserved.
 	</footer>
